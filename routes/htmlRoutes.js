0b6ee1177834f31bee86d68fa68fb232364b7bc8
Allen M <tmracing008@yahoo.com>
2019-01-22 00:46:58 -0800

    add myHome, myFavorites, html routes accordingly, and image api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,39 +5,104 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
-  // app.get("/", function(req, res) {
-  //   db.Recipes.findAll({}).then(function(dbRecipes) {
-  //     res.render("index", {
-  //       title: dbRecipes
-  //     });
-  //   });
-  // });
   // Load page to create a post.
   app.get("/createPost", function(req, res) {
     res.render("createPost");
   });
-  //Home Page for a Logged in User
+  // Home Page for a Logged in User
   app.get("/home", function(req, res) {
-    res.json("Logged in");
+    res.render("myHome");
   });
-  //-------------------------------------------------------
-  //load blog page --- only for test use --- will delete later
-  // app.get("/blog", function(req, res) {
-  //   res.render("recipeBlog");
-  // });
+  // My favorites page for a logged in user
+  app.get("/favorites", function(req, res) {
+    res.render("myFavorites");
+  });
+  // Render Login Page
   app.get("/login", function(req, res) {
     res.render("signInPage");
   });
-  //--------------------------------------------------------
   // Load recipe page and pass in an recipe by id
   app.get("/api/getrecipes/:id", function(req, res) {
     db.Recipes.findAll({ where: { cuisine: req.params.id } }).then(function(
       dbRecipe
     ) {
-      console.log(dbRecipe[0].title);
+      //console.log(dbRecipe[0].title);
       res.render("recipeBlog", {
         Recipes: dbRecipe
       });
+    });
+  });
+  // Routes for the buttons on the grid.
+  app.get("/api/getrecipes/American", function(req, res) {
+    db.Recipes.findAll({
+      where: {
+        cuisine: American
+      }
+    }).then(function(dbRecipe) {
+      res.render("recipeBlog", { Recipes: dbRecipe });
+    });
+  });
+  app.get("/api/getrecipes/Mexican", function(req, res) {
+    db.Recipes.findAll({
+      where: {
+        cuisine: American
+      }
+    }).then(function(dbRecipe) {
+      res.render("recipeBlog", { Recipes: dbRecipe });
+    });
+  });
+  app.get("/api/getrecipes/Italian", function(req, res) {
+    db.Recipes.findAll({
+      where: {
+        cuisine: American
+      }
+    }).then(function(dbRecipe) {
+      res.render("recipeBlog", { Recipes: dbRecipe });
+    });
+  });
+  app.get("/api/getrecipes/Indian", function(req, res) {
+    db.Recipes.findAll({
+      where: {
+        cuisine: American
+      }
+    }).then(function(dbRecipe) {
+      res.render("recipeBlog", { Recipes: dbRecipe });
+    });
+  });
+  app.get("/api/getrecipes/Asain", function(req, res) {
+    db.Recipes.findAll({
+      where: {
+        cuisine: American
+      }
+    }).then(function(dbRecipe) {
+      res.render("recipeBlog", { Recipes: dbRecipe });
+    });
+  });
+  app.get("/api/getrecipes/Breakfast", function(req, res) {
+    db.Recipes.findAll({
+      where: {
+        cuisine: American
+      }
+    }).then(function(dbRecipe) {
+      res.render("recipeBlog", { Recipes: dbRecipe });
+    });
+  });
+  app.get("/api/getrecipes/Dessert", function(req, res) {
+    db.Recipes.findAll({
+      where: {
+        cuisine: American
+      }
+    }).then(function(dbRecipe) {
+      res.render("recipeBlog", { Recipes: dbRecipe });
+    });
+  });
+  app.get("/api/getrecipes/Other", function(req, res) {
+    db.Recipes.findAll({
+      where: {
+        cuisine: American
+      }
+    }).then(function(dbRecipe) {
+      res.render("recipeBlog", { Recipes: dbRecipe });
     });
   });
   // Render 404 page for any unmatched routes
